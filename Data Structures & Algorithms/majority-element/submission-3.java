fd1eec231d5f60a358d591618070c1830cc6da31
Sakshi Bisht <sakshibisht943@gmail.com>
2026-07-04 20:47:00 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int freq=1;
+        int ans=nums[0];
+        for(int i=0;i<nums.length-1;i++){
+            if(freq==0){
+                ans=nums[i];
+                freq=1;
+            }
+            if(nums[i]==ans){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+}
