36019185670a9416d102b6a58f97b80c40a9c0be
Sakshi Bisht <sakshibisht943@gmail.com>
2026-07-30 05:14:26 +0530

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    class pair{
+        String first;
+        int second;
+        pair(String f, int s){
+            first=f;
+            second=s;
+        }
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<pair> q=new LinkedList<>();
+        q.add(new pair(beginWord,1));
+        HashSet<String> st = new HashSet<>(wordList);
+        
+        if(!st.contains(endWord)) return 0;
+
+        while(!q.isEmpty()){
+            String word=q.peek().first;
+            int steps=q.peek().second;
+            q.poll();
+            if(word.equals(endWord)) return steps;
+            for(int i=0;i<word.length();i++){
+                for(char j='a';j<='z';j++){
+                    char[] newword=word.toCharArray();
+                    newword[i]=j;
+                    String newWord= new String(newword);
+                    if(st.contains(newWord)){
+                        st.remove(newWord);
+                        q.add(new pair(newWord,steps+1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
